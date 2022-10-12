e957865d5895a7f296e792410e979840a400004c
A-132001 <abdo01143617687@gmail.com>
2022-10-12 16:47:39 +0200

    Views with Class based views

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,9 @@
 from . import views
 from django.urls import path
 app_name = 'polls'
-urlpatterns =[
-    path("",views.Questions,name="Questions_view"),
-    path("<int:question_id>",views.question_detail,name="detail"),
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('<int:question_id>/results/', views.results, name='results'),
-
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,42 @@
+from operator import ge
+from statistics import mode
+from time import time
 from django.shortcuts import render,get_object_or_404
 from . import models
 from django.http import HttpResponse ,HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 # def firstview(request):
 #     return HttpResponse("Hello World")
 
-def Questions(request):
-    question_list = models.Question.objects.order_by("-pub_time")[:5]
-    # output = ','.join([q.question_text for q in question_list])
-    return render(request,"polls/questions.html",{"latest_question_list":question_list})
+###function based views
+# def Questions(request):
+#     question_list = models.Question.objects.order_by("-pub_time")[:5]
+#     # output = ','.join([q.question_text for q in question_list])
+#     return render(request,"polls/questions.html",{"latest_question_list":question_list})
+# def question_detail(request,question_id):
+#     question = get_object_or_404(models.Question,pk=question_id)
+#     return render(request,'polls/detail.html',{"question":question})
+# def results(request,question_id):
+#     result = get_object_or_404(models.Question,pk=question_id)
+#     return render(request,"polls/results.html",{"question":result})
 
-def question_detail(request,question_id):
-    question = get_object_or_404(models.Question,pk=question_id)
-    return render(request,'polls/detail.html',{"question":question})
+
+class IndexView(generic.ListView):
+    template_name = "polls/questions.html"
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        return models.Question.objects.order_by('-pub_time')[:5]
+class DetailView(generic.DeleteView):
+    model=models.Question
+    template_name = 'polls/detail.html'
+    
+class ResultsView(generic.DetailView):
+    model = models.Question
+    template_name = 'polls/results.html'
+
 
 
 def vote(request, question_id):
@@ -35,6 +58,3 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     
 
-def results(request,question_id):
-    result = get_object_or_404(models.Question,pk=question_id)
-    return render(request,"polls/results.html",{"question":result})
